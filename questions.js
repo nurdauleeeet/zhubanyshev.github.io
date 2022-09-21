ed50e649785afffd0ef3722e3cb0c02ccd0102d9
nurdauleeeet <114095115+nurdauleeeet@users.noreply.github.com>
2022-09-21 22:16:45 +0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Адамзат тарихы дамуының ең алғашқы кезеңі?",
+    answer: "Тас дәуірі",
+    options: [
+      "Тас дәуірі",
+      "Қола дәуірі",
+      "Темір дәуірі",
+      "Энеолит дәуірі"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Археологтарың дерегінше жер бетінде адам пайда болған мерзім?",
+    answer: "3 млн. жыл бұрын",
+    options: [
+      "2 млн. жыл бұрын",
+      "3 млн. жыл бұрын",
+      "5 млн. жыл бұрын",
+      "1 млн. жыл бұрын"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Үш млн. жыл бұрын алғашқы адамдар қалыптасқан аймақ?",
+    answer: "Солтүстік-Шығыс Африка, Алдыңғы және Оңт.Азия",
+    options: [
+      "Азия, Араб түбегі, Кіші Азия",
+      "Еуропа, Ява аралы, Қытай",
+      "Солтүстік-Шығыс Африка, Алдыңғы және Оңт.Азия",
+      "Батыс Африка, Араб түбегі, Қазақстан"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Ғылыми жүйе бойынша тас дәуірі бөлінетін кезеңдер?",
+    answer: "Палеолит, Мезолит, Неолит",
+    options: [
+      "Андрон, Беғазы-Дәндібай",
+      "Палеолит, Мезолит, Неолит",
+      "Олдувей, Ашел, Мустье",
+      "Ерте/кейінгі палеолит"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Адамзат тарихында ең ұзақ уақытқа созылған кезең?",
+    answer: "Палеолит",
+    options: [
+      "Мезолит",
+      "Неолит",
+      "Палеолит",
+      "Энеолит"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
